eab0247de46615522bbb4b97fbc3e4d58610a992
Albert Etsebeth <alberte@AlbertEtsebethMac13Pro.local>
2016-12-15 18:16:45 +0200

    Update 2

--- run_sipp.sh
+++ run_sipp.sh
@@ -7,16 +7,19 @@ fi
 # wait for networking to setup
 sleep 5
 
-ifaces=$(ifconfig | grep "inet " | awk -F'[: ]+' '{print $4}')
-for iface in $ifaces; do
-	if [ "$iface" != "127.0.0.1" ]; then
-		IF=$iface
-	fi
-done
-if [[ -z $IF ]]; then
-	echo "ERROR: a valid interface is not found!"
-	exit 1
-fi
+#Removed as interface will be specified in the $ARGS
+#ifaces=$(ifconfig | grep "inet " | awk -F'[: ]+' '{print $4}')
+#for iface in $ifaces; do
+#	if [ "$iface" != "127.0.0.1" ]; then
+#		IF=$iface
+#	fi
+#done
+#if [[ -z $IF ]]; then
+#	echo "ERROR: a valid interface is not found!"
+#	exit 1
+#fi
 
-cd /data && sipp -bg -trace_stat -fd 1s -trace_rtt -rtt_freq 200 -trace_logs -trace_err -i $IF $ARGS
-tail -f /dev/null
+#cd /data && sipp -bg -trace_stat -fd 1s -trace_rtt -rtt_freq 200 -trace_logs -trace_err -i $IF $ARGS
+#New SIPp command
+cd /data && sipp $ARGS
+#tail -f /dev/null
