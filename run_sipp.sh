32a2311d8883da10cbbe273dc9dd3df31d62911d
Albert Etsebeth <alberte@AlbertEtsebethMac13Pro.local>
2017-01-13 13:50:26 +0200

    Update

--- run_sipp.sh
+++ run_sipp.sh
@@ -10,5 +10,5 @@ sleep 7
 #To-Do: Enable background process with statistics
 #sipp -bg -trace_stat -fd 1s -trace_rtt -rtt_freq 200 -trace_logs -trace_err $ARGS
 #New SIPp with command-line variable
-sipp $ARGS
+sipp -bg $ARGS
 #tail -f /dev/null
